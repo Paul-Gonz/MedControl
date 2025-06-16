823917ffd969d469ff3522702095c145cdef0920
Jose <herediajose04@gmail.com>
2025-06-15 20:13:12 -0400

    Se agrego la plantilla al proyecto

--- MedControl/routes/web.php
+++ MedControl/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::middleware(['web'])
+    ->prefix('admin')
+    ->group(base_path('routes/admin.php'));
