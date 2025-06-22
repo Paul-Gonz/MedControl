b9f674d885f272fad7e29c026524d1ab51560c0f
Paul-Gonz <gonzalezpaul204@gmail.com>
2025-06-21 23:29:18 -0400

    Solucion de error al registrar un usuario

--- MedControl/app/Models/Usuario.php
+++ MedControl/app/Models/Usuario.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     protected $table = 'usuarios';
-   protected $primaryKey = 'usuario_id';
+    protected $primaryKey = 'usuario_id';
     public $incrementing = true;
+    public $timestamps = false;
+
     protected $fillable = [
         'usuario',
         'clave',
