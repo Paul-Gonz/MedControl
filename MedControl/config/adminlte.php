69ebd77937eb1e290ff2a1bac2831e7363fc78d9
Paul-Gonz <gonzalezpaul204@gmail.com>
2025-07-02 12:35:48 -0400

    bvhjgfhtcfjtcmjn

--- MedControl/config/adminlte.php
+++ MedControl/config/adminlte.php
@@ -257,7 +257,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'admin',
+    'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
